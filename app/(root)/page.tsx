6d5a55a45ab80daa819a49bc4275d9d6e5e96ee0
sanjaya007 <paudelsanjaya702@gmail.com>
2023-12-27 00:52:40 +0545

    thread card created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,33 @@
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
   const result = await fetchPosts(1, 30);
-  console.log(result);
-
+  const user = await currentUser();
   return (
     <>
-      <section className="mt-9 flex flex-col gap-10"></section>
+      <section className="mt-9 flex flex-col gap-10">
+        {result?.posts.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result?.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id || ""}
+                parentId={post._parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
